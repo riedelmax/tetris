6d596910e6a8ae71f19a0b773c8b5438a15d4f04
student <student@mct-vm>
2017-06-29 10:58:52 +0200

    Controller angepasst

---
+++ Controller.h
@@ -0,0 +1,22 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "gpio_msp432.h"
+#include "adc14_msp432.h"
+
+class Controller
+{
+    private:
+        gpio_msp432_pin dropButton;
+        gpio_msp432_pin spinButton;
+        adc14_msp432_channel joy_X;
+        float joy_X_Voffset; //VoltageOffset
+
+    public:
+        Controller();
+        bool dropButtonPressed();
+        bool spinButtonPressed();
+        int isMoving();
+};
+
+#endif /* CONTROLLER_H */
